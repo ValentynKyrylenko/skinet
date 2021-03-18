01d3966133e7d1f0b874878a8d49294592ef63a6
Valentyn Kyrylenko <valentynkyrylenko@Valentyns-MacBook-Pro-2.local>
2021-03-18 18:56:45 +0000

    End of sec 12

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { CoreModule } from './core/core.module';
 import { ShopModule } from './shop/shop.module';
 import { HomeModule } from './home/home.module';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { LoadingInterceptor } from './core/interceptors/loading.interceptors';
 
 
 @NgModule({
@@ -25,12 +27,13 @@ import { ErrorInterceptor } from './core/interceptors/error.interceptor';
     HttpClientModule,
     //import core module
     CoreModule,
- 
     HomeModule,
+    NgxSpinnerModule
 
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true},
   ],
   bootstrap: [AppComponent]
 })
--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -12,7 +12,7 @@ import {shopParams} from '../share/Models/shopParams'
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
-  @ViewChild('search', {static: true}) searchTerm: ElementRef;
+  @ViewChild('search', {static: false}) searchTerm: ElementRef;
   //consume a service which we created
   products: IProduct[];
   brands: IBrand[];
