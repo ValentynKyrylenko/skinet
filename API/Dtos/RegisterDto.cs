52088db6564033fd73f5d017223ec1d79da984ea
Valentyn Kyrylenko <valentynkyrylenko@valentyns-mbp-2.home>
2021-04-05 13:35:43 +0100

    End of s 16

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName {get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$", ErrorMessage = "Pasword must have one Upper Case 1 Lower case, 1 Number and 1 Long alfpha numeric and at least 6 char")]
         public string Password { get; set; }
     }
 }
